05420d91ac1e583adcde8efcec48f84d06f9da88
T D <td@localhost.localdomain>
2022-07-22 22:08:12 +0300

    Load GDT, move to protected mode

--- Makefile
+++ Makefile
@@ -2,5 +2,3 @@ BOOTBIN = ./bootloader/boot.bin
 
 all:
 	nasm -f bin bootloader/boot.asm -o $(BOOTBIN)
-	dd if=./message.txt >> $(BOOTBIN)
-	dd if=/dev/zero bs=512 count=1 >> $(BOOTBIN)
--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,62 +1,76 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 _start:
     jmp short start
     nop
-
-times 33 db 0
+    times 33 db 0
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
-    cli ; Clear Interrupts
-    mov ax, 0x7c0
+    cli ; Clear interrupts, so there won't be any while we change the segments
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0
     mov ss, ax
     mov sp, 0x7c00
-    sli ; Enables Interrupts
+    sti ; Enable interrupts
 
-    mov ah, 2 ; READ SECTOR COMMAND
-    mov al, 1 ; ONE SECTOR TO READ
-    mov ch, 0 ; Cylinder low 8 bits
-    mov cl, 2 ; READ SECTOR 2
-    mov dh, 0 ; HEAD NUMBER
-    mov bx, buffer ; Where to read
-    int 0x13
-    jc error
+load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov si, buffer
-    call print
+; GDT
+gdt_start:
+; Null descriptor, place holder (some emulators use it)
+gdt_null:
+    dd 0x0  
+    dd 0x0
+
+; offset 0x8
+gdt_code:           ; For Code Segment
+    dw 0xffff        ; segment limit first 0-15 bits
+    dw 0x0            ; base first 0-15 bits
+    db 0x0            ; base 16-23 bits
+    db 0x9a            ; access byte
+    db 0b11001111    ; high 4 bits (flags) low 4 bits (limit 4 last bits)(limit is 20 bit wide)
+    db 0x0            ; base 24-31 bits
+
+; offset 0x10
+gdt_data:            ; DS, SS, ES, FS GS
+    dw 0xffff        ; segment limit first 0-15 bits
+    dw 0x0            ; base first 0-15 bits
+    db 0x0            ; base 16-23 bits
+    db 0x92            ; access byte
+    db 0b11001111    ; high 4 bits (flags) low 4 bits (limit 4 last bits)(limit is 20 bit wide)
+    db 0x0            ; base 24-31 bits
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; Size of the gdt
+    dd gdt_start ; Pointer to the beginning of the GDT
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+    
     jmp $
 
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-print:
-.loop:
-    mov bx, 0
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to load sector', 0
-
-times 510-($ - $$) db 0
+times 510-($ - $$) db 0 ; 510 - (current address - beginning address)
 dw 0xAA55
-
-buffer:
