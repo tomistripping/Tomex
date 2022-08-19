b11a101f2514ea3990de15ebc6962610098e95f5
T D <td@localhost.localdomain>
2022-08-19 20:52:25 +0300

    Function for building text mode chars +stdint.h

--- src/kernel.c
+++ src/kernel.c
@@ -1,15 +1,12 @@
 #include "kernel.h"
+#include <stdint.h>
+
+uint16_t terminal_make_char(char c, char color) {
+    return (color << 8) | c;
+}
 
 void kernel_main() {
-    char* video_mem = (char*)(0xB8000);
-    video_mem[0] = 'T';
-    video_mem[1] = 1;
-    video_mem[2] = 'o';
-    video_mem[3] = 2;
-    video_mem[4] = 'm';
-    video_mem[5] = 3;
-    video_mem[6] = 'e';
-    video_mem[7] = 4;
-    video_mem[8] = 'r';
-    video_mem[9] = 5;
+    uint16_t* video_mem = (uint16_t*)(0xB8000);
+    video_mem[0] = terminal_make_char('T', 5);
+    video_mem[1] = terminal_make_char('a', 3);
 }
